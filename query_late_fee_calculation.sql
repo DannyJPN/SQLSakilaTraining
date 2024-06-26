463f3af16c5f64e6e087bc2a644e42c2813e753b
DannyJPN <krupadan@email.cz>
2024-06-26 13:57:46 +0200

    finished

--- query_late_fee_calculation.sql
+++ query_late_fee_calculation.sql
@@ -1,28 +1,23 @@
--- query_late_fee_calculation.sql
-
-/*
-This script calculates the late fee for rentals. The late fee is calculated as 1% of the rental rate for each day past the due date.
-Assume films are rented for 14 days. The query returns the rental ID, customer ID, film title, rental date, due date, return date, days late, and the calculated late fee.
-The query handles cases where the film has not been returned yet.
-*/
-
+DECLARE @due_length Int =14
+SELECT * from(
 SELECT
     r.rental_id,
     r.customer_id,
     f.title AS film_title,
     r.rental_date,
-    DATEADD(DAY, 14, r.rental_date) AS due_date,
+	f.rental_rate,
+    DATEADD(DAY, @due_length, r.rental_date) AS due_date,
     r.return_date,
-    DATEDIFF(DAY, DATEADD(DAY, 14, r.rental_date), ISNULL(r.return_date, GETDATE())) AS days_late,
+    DATEDIFF(DAY, DATEADD(DAY, @due_length, r.rental_date), ISNULL(r.return_date, GETDATE())) AS days_late,
     CASE
-        WHEN r.return_date IS NULL THEN DATEDIFF(DAY, DATEADD(DAY, 14, r.rental_date), GETDATE()) * (f.rental_rate * 0.01)
-        ELSE DATEDIFF(DAY, DATEADD(DAY, 14, r.rental_date), r.return_date) * (f.rental_rate * 0.01)
+        WHEN r.return_date IS NULL THEN DATEDIFF(DAY, DATEADD(DAY, @due_length, r.rental_date), GETDATE()) * (f.rental_rate * 0.01)
+        ELSE DATEDIFF(DAY, DATEADD(DAY, @due_length, r.rental_date), r.return_date) * (f.rental_rate * 0.01)
     END AS late_fee
 FROM
-    rental r
+    sakila.dbo.rental r
 JOIN
-    inventory i ON r.inventory_id = i.inventory_id
+    sakila.dbo.inventory i ON r.inventory_id = i.inventory_id
 JOIN
-    film f ON i.film_id = f.film_id
+    sakila.dbo.film f ON i.film_id = f.film_id) as result_tab
 WHERE
-    DATEDIFF(DAY, DATEADD(DAY, 14, r.rental_date), ISNULL(r.return_date, GETDATE())) > 0;
+    result_tab.late_fee>0;
--- query_recent_rentals.sql
+++ query_recent_rentals.sql
@@ -1,9 +1,3 @@
--- query_recent_rentals.sql
-
-/*
-This script retrieves information about all customers who rented a film in the last year.
-The query returns the customer's first name, last name, and complete address (including address, address2, district, city, postal code, and country).
-*/
 
 SELECT distinct
     c.first_name,
@@ -24,5 +18,5 @@ JOIN
 JOIN
     sakila.dbo.country co ON ci.country_id = co.country_id
 WHERE
-    year(r.rental_date) >= 2006/* DATEADD(YEAR, -1, GETDATE());*/
+    year(r.rental_date) >=  DATEADD(YEAR, -1, GETDATE());
 	/*replace with year 2006 to see effect - for "current year" there is no record*/
